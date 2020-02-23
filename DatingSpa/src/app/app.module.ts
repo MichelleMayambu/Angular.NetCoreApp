386597b196e4c5a747463dd998e44a5f23da0450
MichelleMayambu <your_email@example.com>
2020-02-23 17:37:52 +0200

    added httclientModuletoApp

--- DatingSpa/src/app/app.module.ts
+++ DatingSpa/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValuesComponent } from './values/values.component';
-
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
    declarations: [
       AppComponent,
       ValuesComponent
    ],
    imports: [
-      BrowserModule
+      BrowserModule,
+      HttpClientModule
    ],
    providers: [],
    bootstrap: [
