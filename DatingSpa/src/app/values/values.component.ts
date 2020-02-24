07719baf00cbb9e2b08c7a968b5b619c0ffe3a3e
MichelleMayambu <your_email@example.com>
2020-02-24 08:44:19 +0200

    added cross origin policy as service and middleware in api

--- DatingSpa/src/app/values/values.component.ts
+++ DatingSpa/src/app/values/values.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-values',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./values.component.css']
 })
 export class ValuesComponent implements OnInit {
+  values: any;
 
-  constructor() { }
+  //use dependency injection to use Http clientModule service
+  constructor(private  http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
-
+  getValues(){
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+    }, error =>{
+      console.log(error);
+    });
+  }
 }
--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite
             (Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();  //add cross origin as a service
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +42,7 @@ namespace DatingApp.API
             }
 
             //  app.UseHttpsRedirection();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
 
             app.UseAuthorization();
