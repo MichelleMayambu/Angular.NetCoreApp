430f85fd73eef9d9d5b63e6698a92dd0af1302ce
MichelleMayambu <your_email@example.com>
2020-02-25 13:44:34 +0200

    addedHashSaltandHashPasswordToUserRegistration

--- Models/User.cs
+++ Models/User.cs
@@ -2,12 +2,13 @@ namespace Angular.NetCoreApp.Models
 {
     public class User
     {
+     
         public int Id {get; set;}
 
         public string UserName {get; set;}
 
-        public byte PasswordHash {get; set;}
+        public byte[] PasswordHash {get; set;}
 
-        public byte PasswordSalt {get; set;}
+        public byte[] PasswordSalt {get; set;}
     }
 }
